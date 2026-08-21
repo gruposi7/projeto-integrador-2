457a3d9065c52d2744ed4df51e7ceac88fcb905d
Janku <sicurso8@gmail.com>
2026-08-20 23:18:25 -0300

    Corrige entrega do frontend na Vercel

--- api/[...path].js
+++ api/[...path].js
@@ -1,6 +1,19 @@
 const app = require('../server');
+const path = require('path');
 
 module.exports = (req, res) => {
+  const caminho = req.url.split('?')[0];
+  const arquivosPublicos = {
+    '/': 'index.html',
+    '/index.html': 'index.html',
+    '/style.css': 'style.css',
+    '/java.js': 'java.js',
+  };
+
+  if (arquivosPublicos[caminho]) {
+    return res.sendFile(path.join(__dirname, '..', arquivosPublicos[caminho]));
+  }
+
   if (!req.url.startsWith('/api')) {
     req.url = `/api${req.url === '/' ? '' : req.url}`;
   }
